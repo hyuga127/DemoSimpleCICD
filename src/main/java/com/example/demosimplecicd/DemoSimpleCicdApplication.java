8a4f81ffbea3ab4cc3b6c6b7deec2646a8da3edd
hyuga127 <tranhuyzaza@gmail.com>
2025-04-22 19:44:11 +0700

    first commit

---
+++ src/main/java/com/example/demosimplecicd/DemoSimpleCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.demosimplecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSimpleCicdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoSimpleCicdApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demosimplecicd/DemoSimpleCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demosimplecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSimpleCicdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
